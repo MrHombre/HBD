0da1438e7b2c91b9b90dc5a78eeda476972a4e27
James Lett <jlett89@gmail.com>
2016-07-11 10:21:51 -0700

    just more changes to the index

--- js/index.js
+++ js/index.js
@@ -10,7 +10,7 @@ var w = c.width = window.innerWidth,
 		hh = h / 2,
 		
 		opts = {
-			strings: [ 'HAPPY', 'BIRTHDAY!' ],
+			strings: [ 'HAPPY', 'BIRTHDAY', 'ELIZABETH!' ],
 			charSize: 30,
 			charSpacing: 35,
 			lineHeight: 40,
